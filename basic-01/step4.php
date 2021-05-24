5bedfa99b669b83077322ba5023c66cb0c811a4a
Hasan1Aref <Hasan.Aref.eng@gmail.com>
2021-05-24 11:55:51 +0300

    step4

---
+++ basic-01/step4.php
@@ -0,0 +1,8 @@
+<?php 
+$name="Gaby";
+$position=" tech consultant";
+$github_url=  "https://github.com/gk-git";
+echo "Hello, I'm ".$name.", I'm a ".$position." please check my github link".$github_url;
+echo "Hello, I'm $name , I'm a $position please check my github link $github_url";
+
+?>
