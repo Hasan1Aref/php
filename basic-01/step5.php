36a416c1f327eb1332c37e48652fd0b766725df1
Hasan1Aref <Hasan.Aref.eng@gmail.com>
2021-05-24 12:32:49 +0300

    step5

---
+++ basic-01/step5.php
@@ -0,0 +1,10 @@
+<?php 
+  $course_name = "php";
+  $enrolled_students =  20;
+  $price =  150;
+  $on_discount =  true;
+   echo  "Course title:$course_name";
+   echo " <br> Enrolled Students:$enrolled_students";
+   echo " <br> Course price:$price";
+   echo " <br> Course on discount:$on_discount";
+?>
