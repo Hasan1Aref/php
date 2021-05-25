659816e25797713e60197e5279c9298f8a8e5ab7
Hasan1Aref <Hasan.Aref.eng@gmail.com>
2021-05-25 10:46:05 +0300

    step5

---
+++ basic-02/step5.php
@@ -0,0 +1,19 @@
+<?php
+function wordToDigit($word){
+
+    switch($word){
+        case 'zero':
+            echo 0;break;
+            case 'one':
+                echo 1;break;
+                case 'five':
+                    echo 5;break;
+    }
+}
+wordToDigit(one);
+
+
+
+
+
+?>
